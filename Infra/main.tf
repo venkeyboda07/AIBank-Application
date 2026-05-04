582748be78711217ef429a43bca55857f8d33ba5
Boda Venkatesh <venkeyboda02@gmail.com>
2026-05-04 14:36:46 +0530

    modified terraform Infra

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = var.region
 }
 
+# ---------------- VPC ----------------
+
 resource "aws_vpc" "bankapp_aws_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -49,6 +51,8 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.bankapp_aws_route_table.id
 }
 
+# ---------------- Security Groups ----------------
+
 resource "aws_security_group" "bankapp_aws_cluster_sg" {
   vpc_id = aws_vpc.bankapp_aws_vpc.id
 
@@ -57,10 +61,6 @@ resource "aws_security_group" "bankapp_aws_cluster_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "bankapp_aws-cluster-sg"
   }
 }
 
@@ -80,11 +80,68 @@ resource "aws_security_group" "bankapp_aws_node_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "bankapp_aws-node-sg"
-  }
 }
+
+# ---------------- IAM ----------------
+
+resource "aws_iam_role" "bankapp_aws_cluster_role" {
+  name = "bankapp_aws-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.bankapp_aws_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "bankapp_aws_node_group_role" {
+  name = "bankapp_aws-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# ---------- Node policies ---------
+resource "aws_iam_role_policy_attachment" "node_policy" {
+  role       = aws_iam_role.bankapp_aws_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.bankapp_aws_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "registry_policy" {
+  role       = aws_iam_role.bankapp_aws_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# ---------------EBS CSI permission ---------
+resource "aws_iam_role_policy_attachment" "ebs_policy" {
+  role       = aws_iam_role.bankapp_aws_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# ---------------- EKS Cluster ----------------
 
 resource "aws_eks_cluster" "bankapp_aws" {
   name     = "bankapp_aws-cluster"
@@ -94,13 +151,35 @@ resource "aws_eks_cluster" "bankapp_aws" {
     subnet_ids         = aws_subnet.bankapp_aws_subnet[*].id
     security_group_ids = [aws_security_group.bankapp_aws_cluster_sg.id]
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_policy
+  ]
 }
+
+# ---------------- Launch Template (IMDS) ----------------
+
+resource "aws_launch_template" "bankapp_lt" {
+  name_prefix = "bankapp-lt"
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "optional" 
+  }
+}
+
+# ---------------- Node Group ----------------
 
 resource "aws_eks_node_group" "bankapp_aws" {
   cluster_name    = aws_eks_cluster.bankapp_aws.name
   node_group_name = "bankapp_aws-node-group"
   node_role_arn   = aws_iam_role.bankapp_aws_node_group_role.arn
   subnet_ids      = aws_subnet.bankapp_aws_subnet[*].id
+
+  launch_template {
+    id      = aws_launch_template.bankapp_lt.id
+    version = "$Latest"
+  }
 
   scaling_config {
     desired_size = 2
@@ -110,67 +189,18 @@ resource "aws_eks_node_group" "bankapp_aws" {
 
   instance_types = ["t3.medium"]
 
-  remote_access {
-    ec2_ssh_key               = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.bankapp_aws_node_sg.id]
-  }
-}
-
-resource "aws_iam_role" "bankapp_aws_cluster_role" {
-  name = "bankapp_aws-cluster-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
+  depends_on = [
+    aws_eks_cluster.bankapp_aws
   ]
 }
-EOF
-}
 
-resource "aws_iam_role_policy_attachment" "bankapp_aws_cluster_role_policy" {
-  role       = aws_iam_role.bankapp_aws_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
+# ---------------- EBS CSI DRIVER (AUTO INSTALL) ----------------
 
-resource "aws_iam_role" "bankapp_aws_node_group_role" {
-  name = "bankapp_aws-node-group-role"
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = aws_eks_cluster.bankapp_aws.name
+  addon_name   = "aws-ebs-csi-driver"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
+  depends_on = [
+    aws_eks_node_group.bankapp_aws
   ]
 }
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "bankapp_aws_node_group_role_policy" {
-  role       = aws_iam_role.bankapp_aws_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "bankapp_aws_node_group_cni_policy" {
-  role       = aws_iam_role.bankapp_aws_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "bankapp_aws_node_group_registry_policy" {
-  role       = aws_iam_role.bankapp_aws_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
